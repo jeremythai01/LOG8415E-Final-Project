3416dc51b229c211d055794d4a457e638ed4b621
Jeremy Thai <github@jetha.anonaddy.com>
2022-12-06 20:54:54 -0500

    Created new class to handle remote ssh connections

---
+++ remote_client.py
@@ -0,0 +1,38 @@
+import paramiko
+import time
+
+class RemoteClient:
+
+    def __init__(self, hostname, username, private_keyfile):
+        private_key = paramiko.RSAKey.from_private_key_file(private_keyfile)
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        time.sleep(30)
+
+        self.ssh_client.connect(hostname=hostname, username=username, pkey=private_key)
+
+        # Open SFTP 
+        self.ftp_client=self.ssh_client.open_sftp()
+
+    
+    def execute_command(self, command):
+        stdin, stdout, stderr = self.ssh_client.exec_command(command)
+
+        exit_status = stdout.channel.recv_exit_status()  # Blocking call
+        if exit_status != 0:
+            print("Error", exit_status)
+            
+
+    def close_connection(self):
+        self.ssh_client.close()
+
+
+    def get_file(self, remote_filepath, local_filepath):
+        self.ftp_client.get(remote_filepath, local_filepath)
+
+
+    def upload_file(self, local_filepath, remote_filepath):
+        self.ftp_client.put(local_filepath, remote_filepath)
+
+
