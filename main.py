6c7a862066b3f0ff2a39bc5fe446ec299e743cb9
Jeremy Thai <github@jetha.anonaddy.com>
2022-12-08 00:28:16 -0500

    Create instances for cluster manager and nodes

--- main.py
+++ main.py
@@ -41,11 +41,44 @@ print("executing mysql standalone setup remotely...")
 standalone_remote_client.execute_command("sh setup_standalone_mysql.sh")
 
 
-print("Executing benchmark script remotely...")
-standalone_remote_client.execute_command("sh setup_benchmark.sh")
+#Create ec2 instance for MySQL cluster manager
+print('Creating instance for MySQL cluster manager...')
+manager_instance_id = ec2_creator.create_instance(
+    availability_zone=constant.US_EAST_1A,
+    instance_type=constant.T2_MICRO)
+print(f'Instance {manager_instance_id} is running!')
 
-print("getting back mysql standalone benchmark result from remote server...")
-standalone_remote_client.get_file(
-    remote_filepath='benchmark_results.txt',
-    local_filepath='results/standalone_benchmark_results.txt', 
-    )
+# Create ec2 instance for MySQL cluster data node
+print('Creating instance for MySQL data node 1...')
+node1_instance_id = ec2_creator.create_instance(
+    availability_zone=constant.US_EAST_1A,
+    instance_type=constant.T2_MICRO)
+print(f'Instance {node1_instance_id} created!')
+
+# Create ec2 instance for MySQL cluster data node
+print('Creating instance for MySQL data node 2...')
+node2_instance_id = ec2_creator.create_instance(
+    availability_zone=constant.US_EAST_1A,
+    instance_type=constant.T2_MICRO)
+print(f'Instance {node2_instance_id} created!')
+
+
+# Create ec2 instance for MySQL cluster data node
+print('Creating instance for MySQL data node 3...')
+node3_instance_id = ec2_creator.create_instance(
+    availability_zone=constant.US_EAST_1A,
+    instance_type=constant.T2_MICRO)
+print(f'Instance {node3_instance_id} created!')
+
+
+manager_public_dns, manager_private_dns = ec2_creator.get_instance_dns(manager_instance_id)
+print(manager_public_dns, manager_private_dns)
+
+node1_public_dns, node1_private_dns = ec2_creator.get_instance_dns(node1_instance_id)
+print(node1_public_dns, node1_private_dns)
+
+node2_public_dns, node2_private_dns = ec2_creator.get_instance_dns(node2_instance_id)
+print(node2_public_dns, node2_private_dns)
+
+node3_public_dns, node3_private_dns = ec2_creator.get_instance_dns(node3_instance_id)
+print(node3_public_dns, node3_private_dns)
