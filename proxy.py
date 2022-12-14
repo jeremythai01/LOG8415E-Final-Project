713d9c91522bc708ed2daf0dab74fb932d9bcc8b
Jeremy Thai <github@jetha.anonaddy.com>
2022-12-14 11:08:25 -0500

    Added direct hit, random and custom forwarding

--- proxy.py
+++ proxy.py
@@ -1,5 +1,7 @@
 from database_connector import DBConnector
 from sshtunnel import SSHTunnelForwarder
+from pythonping import ping
+import random
 
 class Proxy:
 
@@ -13,4 +15,30 @@ class Proxy:
 
     def forward_request(self, target_host, query):
         with SSHTunnelForwarder(target_host, ssh_username='ubuntu', ssh_pkey=self.pkey, remote_bind_address=(self.manager_pdns, 3306)) as tunnel:
-            DBConnector(self.manager_pdns).execute_query(query)
+            DBConnector(self.manager_pdns).execute_query(query)     
+
+        
+    def direct_hit(self, query):
+        self.forward_request(self.manager_pdns, query)
+
+
+    # Random: randomly choose a slave node 
+    def random(self, query):
+
+        # Choose randomly a slave node
+        target_host = random.choice([self.node1_pdns, self.node2_pdns, self.node3_pdns])
+
+        self.forward_request(target_host, query)
+
+
+    def ping_host(self, host):
+        return ping(target=host, count=1, timeout=2).rtt_avg_ms
+        
+
+    # Customize: Measure ping for all server and choose the one with the smallest response time
+    def customize(self, query):
+
+        nodes = [self.node1_pdns, self.node2_pdns, self.node3_pdns]
+        avg_latencies = [self.ping_host(host) for host in nodes]
+        fastest_node = nodes[avg_latencies.index(min(avg_latencies))]
+        self.forward_request(fastest_node, query)
