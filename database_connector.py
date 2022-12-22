d67f1cf60d1e37a1c2e272e87b89f50a5a8df6b1
Jeremy Thai <github@jetha.anonaddy.com>
2022-12-22 00:24:22 -0500

    Documented DB connector class

--- database_connector.py
+++ database_connector.py
@@ -1,31 +1,53 @@
-import pymysql.cursors
+import pymysql
+import constant
+
 
 class DBConnector:
-
-    def __init__(self, manager_ip):
-        self.connection = self.create_connection(manager_ip, 3306, 'user', 'password', 'sakila')
+    """Database connector used to connect application to mysql database."""
+    def __init__(self, manager_pdns):
+        """Constructor
+        
+        Parameters
+        ----------
+        manager_pdns :  string
+                        Private DNS of cluster manager
+        """
+        self.connection = self.create_connection(manager_pdns)
         pass
 
 
-    def create_connection(self, host, port, user, password, database):
-
+    def create_connection(self, host):
+        """Create connection to the MySQL database.
+        
+        Parameters
+        ----------
+        host :  string
+                The host where the database server is located
+       
+        Returns
+        -------
+        connection : created database connection
+        """
         # Connect to the database
         connection = pymysql.connect(host=host,
-                                    port=port,
-                                    user=user,
-                                    password=password,
-                                    database=database,
+                                    port=constant.MYSQL_PORT,
+                                    user=constant.MYSQL_USERNAME,
+                                    password=constant.MYSQL_PASSWORD,
+                                    database='sakila',
                                     autocommit=True)
 
         return connection
 
 
     def execute_query(self, query):
-
+        """Execute SQL query using database connection cursor.
+        
+        Parameters
+        ----------
+        query : string
+                query to be executed
+        """
         with self.connection:
             with self.connection.cursor() as cursor:
-                # Create a new record
-                #sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        
                 cursor.execute(query)
                 print(cursor.fetchall())
