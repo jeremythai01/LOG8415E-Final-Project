2a440b237910e5030ea79f1530cf1a5c4ad1eaf3
Jeremy Thai <github@jetha.anonaddy.com>
2022-12-22 00:25:22 -0500

    Documented utils

--- utils.py
+++ utils.py
@@ -1,24 +1,25 @@
 from remote_client import RemoteClient
+from logger import logger
 import constant
 
+def setup_node(private_keyfile, node_public_dns, node_id):
+    """Setup data node remotely using RemoteClient object
+        
+        Parameters
+        ----------
+        private_keyfile :   string
+                            File path of private key
+        node_public_dns :   string
+                            Public DNS of cluster data node 
+        node_id :           int
+                            ID of node (1,2,3)
+        """
+    logger.info(f"Setting up data node {node_id}...")
 
-def setup_node(private_keyfile, node_public_dns):
+    node_client = RemoteClient(hostname=node_public_dns, username=constant.USERNAME, private_keyfile=private_keyfile)
 
-    node_client = RemoteClient(
-        hostname=node_public_dns, 
-        username=constant.USERNAME, 
-        private_keyfile=private_keyfile
-    )
-
-    node_client.upload_file(
-    local_filepath='setups/setup_cluster_node.sh', 
-    remote_filepath='/home/ubuntu/setup_cluster_node.sh'
-    )
-
-    node_client.upload_file(
-    local_filepath='configs/my.cnf', 
-    remote_filepath='/home/ubuntu/my.cnf'
-    )
+    node_client.upload_file(local_filepath='setups/setup_cluster_node.sh', remote_filepath='/home/ubuntu/setup_cluster_node.sh')
+    node_client.upload_file(local_filepath='configs/my.cnf', remote_filepath='/home/ubuntu/my.cnf')
 
     node_client.execute_command("sh setup_cluster_node.sh")
 
