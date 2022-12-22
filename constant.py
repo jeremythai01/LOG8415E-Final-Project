602891728edd365a6770d9fe4ac75c3427d6c1c8
Jeremy Thai <github@jetha.anonaddy.com>
2022-12-22 00:18:56 -0500

    Added constants for proxy

--- constant.py
+++ constant.py
@@ -2,6 +2,7 @@
 UBUNTU_IMAGE = 'ami-08c40ec9ead489470'
 US_EAST_1A = 'us-east-1a'
 T2_MICRO = 't2.large'
+T2_LARGE = 't2.large'
 USERNAME = 'ubuntu'
 
 # Port configuration
@@ -9,3 +10,8 @@ DEFAULT_SECURITY_GROUP_NAME = 'default'
 CIDR_IP = '0.0.0.0/0'
 SSH_PORT = 22
 IP_PROTOCOL = 'tcp'
+
+# PROXY
+MYSQL_PORT = 3306
+MYSQL_USERNAME = 'proxy'
+MYSQL_PASSWORD = 'pwd'
